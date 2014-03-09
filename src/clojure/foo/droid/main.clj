09a96084fd3b2077bfe6491935926bbf8e8b5dc6
bprzygoda <bprzygoda@gmail.com>
2014-03-09 17:22:53 +0100

    Stopped using global var for activity

--- src/clojure/foo/droid/main.clj
+++ src/clojure/foo/droid/main.clj
@@ -2,10 +2,11 @@
   (:use [neko.activity :only [defactivity set-content-view!]]
         [neko.threading :only [on-ui post]]
         [neko.ui :only [make-ui]]
-        [neko.notify :only [toast]]
+        [neko.notify :only [toast fire notification]]
         [neko.log :as log])
   (:import com.google.android.gms.common.GooglePlayServicesUtil
            com.google.android.gms.gcm.GoogleCloudMessaging
+           android.view.View
            android.os.AsyncTask
            java.lang.Thread))
 
@@ -13,8 +14,6 @@
   (GooglePlayServicesUtil/isGooglePlayServicesAvailable context))
 
 (def sender-id "617220181631")
-
-(def a)
 
 (defn on-background [f s]
   (let [task (proxy [AsyncTask] []
@@ -30,17 +29,17 @@
   (let [gcm (GoogleCloudMessaging/getInstance context)]
     (.register gcm (into-array String [sender-id]))))
 
-(def ui [:linear-layout {}
-         [:text-view {:text "Hello from Clojure!!!"}]
-         [:button {:text "bash it"
-                   :on-click (fn [w]
-                               (let [tb (.findViewById a 1)]
-                                 (.setText tb @regid)))}]
-         [:edit-text {:id 1}]])
+(def ui (make-ui [:linear-layout {:id-holder true}
+                  [:text-view {:text "Hello from Clojure!!!"}]
+                  [:button {:text "show id"
+                            :on-click (fn [w]
+                                        (let [tb (::regid-txt (.getTag ui))]
+                                          (.setText tb @regid)))}]
+                  [:edit-text {:id ::regid-txt}]]))
 
-(defn register-in-background []
+(defn register-in-background [context]
   (on-background
-   (fn [] (register a))
+   (fn [] (register context))
    (fn [r]
      (reset! regid r))))
 
@@ -48,7 +47,6 @@
   :def a
   :on-create
   (fn [this bundle]
-    (register-in-background)
+    (register-in-background this)
     (on-ui
-     (set-content-view! a
-      (make-ui ui)))))
+     (set-content-view! a ui))))
